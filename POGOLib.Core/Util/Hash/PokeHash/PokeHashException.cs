23a0478d929089b99b233320555951a3df495278
Furtif <msntchat@hotmail.fr>
2017-12-29 20:36:48 +0100

    prj use

---
+++ POGOLib.Core/Util/Hash/PokeHash/PokeHashException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POGOLib.Official.Util.Hash.PokeHash
+{
+    public class PokeHashException : Exception
+    {
+        public PokeHashException(string message) : base(message)
+        {
+
+        }
+    }
+}
