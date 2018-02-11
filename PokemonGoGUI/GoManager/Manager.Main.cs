65423a19d28c0ac7fbd4391ab17b5c9bce9371db
Furtif <msntchat@hotmail.fr>
2018-02-11 21:57:17 +0100

    not nedded

--- PokemonGoGUI/GoManager/Manager.Main.cs
+++ PokemonGoGUI/GoManager/Manager.Main.cs
@@ -591,7 +591,7 @@ namespace PokemonGoGUI.GoManager
                                     if (Snipe.Success)
                                     {
                                         await Task.Delay(CalculateDelay(UserSettings.GeneralDelay, UserSettings.GeneralDelayRandom));
-                                        pokestopsToFarm.Clear();
+                                        //pokestopsToFarm.Clear();
                                         continue;
                                     }
                                 }
